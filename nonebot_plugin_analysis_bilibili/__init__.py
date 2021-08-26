cf15d6fec6cbb1dc16ace1bf9feff633f67fdaac
jjnnnh <qq790943709@sina.com>
2021-08-26 20:39:43 +0800

    Add files via upload

--- nonebot_plugin_analysis_bilibili/__init__.py
+++ nonebot_plugin_analysis_bilibili/__init__.py
@@ -10,13 +10,11 @@ analysis_bili = on_regex(r"((b23|acg).tv)|(bili(22|23|33|2233).cn)|(bilibili(://
 @analysis_bili.handle()
 async def analysis_main(bot: Bot, event: Event, state: dict):
     text = str(event.message).strip()
-    msg = LJLX = ""
+    msg = ""
     if re.search(r"((b23|acg).tv)|(bili(22|23|33|2233).cn)", text, re.I):
         text = await b23_extract(text)
-        LJLX = "短"
     elif re.search(r"bilibili(://|%3A%2F%2F)", text, re.I):
         text = await bili_url(parse.unquote(text))
-        LJLX = "APP"
     if text:
         if re.search(r"(live.bilibili.com)|(bilibili.com/(video|read|bangumi))|(^(av|cv|ep|ss|md)(\d+))|(^BV([a-zA-Z0-9]){10})", text, re.I):
             try:
@@ -24,8 +22,6 @@ async def analysis_main(bot: Bot, event: Event, state: dict):
             except:
                 group_id = f"i{event.user_id}"
             msg = await bili_keyword(group_id, text)
-        elif LJLX:
-            msg = f"{LJLX}链接解析成功，但不支持\n{text}"
     if msg:
         try:
             await analysis_bili.send(msg)
